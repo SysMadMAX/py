637f0baca1d94dbbee4524df8251b9465119d497
Vladimir <maksim-gulko@mail.ru>
2020-08-24 15:12:17 +0300

    first commit

---
+++ lambda_funcs.py
@@ -0,0 +1,21 @@
+# add = lambda x, y: x + y
+# print(add(5, 3))
+
+
+
+
+print((lambda x, y: x + y)(5,3))
+
+
+
+def make_adder(n):
+    return lambda x: x + n
+
+
+plus_3 = make_adder(3)
+plus_5 = make_adder(5)
+
+print(plus_3(4))
+
+print(plus_5(4))
+
